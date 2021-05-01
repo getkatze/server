b100b7cc9383b0010b94c97bd54afe9c95adb32c
Will Lane <williamlane923@gmail.com>
2021-05-01 01:18:47 -0700

    clear classifiers and sentiment

--- resolvers/AnalysisResolver.ts
+++ resolvers/AnalysisResolver.ts
@@ -1,4 +1,4 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql';
+import { Arg, Mutation, Query, Resolver, Float } from 'type-graphql';
 import natural from 'natural';
 
 let classifier = new natural.BayesClassifier();
@@ -12,8 +12,19 @@ export default class AnalysisResolver {
     return true;
   }
 
+  @Mutation(() => Boolean)
+  async clearClassifiers() {
+    classifier = new natural.BayesClassifier();
+  }
+
   @Query(() => String)
   async classify(@Arg('sentence') sentence: string) {
     return classifier.classify(sentence);
+  }
+
+  @Query(() => Float)
+  async sentiment(@Arg('sentence') sentence: string, @Arg('language') language: string) {
+    let analyzer = new natural.SentimentAnalyzer(language, natural.PorterStemmer, 'afinn');
+    return analyzer.getSentiment(new natural.WordTokenizer().tokenize(sentence));
   }
 }
